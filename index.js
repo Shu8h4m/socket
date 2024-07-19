228567a29b08df4f4241878ab903a5d24fda228f
Shubham Bhoyar <its.me.shubham6866@gmail.com>
2024-07-19 17:38:36 +0530

    finald

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(process.env.PORT || 8900,{
     cors:{
-        origin : "https://669a4f4e9413771894f1e40a--inquisitive-tapioca-66bcb6.netlify.app"
+        origin : "https://669a53c59171a62b6037a6f5--resplendent-kitsune-5efe7a.netlify.app"
     },
 });
 let users = [];
